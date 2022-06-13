04f127c1aec51581f653ffa49ab597b9118c6b94
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-06-13 19:48:38 +0200

    useCOntext ok + toggle button x

--- src/components/SIgnUpModal.js
+++ src/components/SIgnUpModal.js
@@ -1,82 +1,89 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { UserContext } from "../context/userContext";
 
-function SignUpModal() {
+export default function SignUpModal() {
+    // j'utilise le hook pour ytransmettre le contexte, et dedans je prends toggleModals + modalState
+    const { modalState, toggleModals } = useContext(UserContext);
+    console.log(modalState, toggleModals);
+
     return (
         <>
-            <div className="position-fixed top-0 vw-100 vh-100">
-                <div
-                    // onClick={closeModal}
-                    className="w-100 h-100 bg-dark bg-opacity-75">
-                </div>
-                <div
-                    className="position-absolute top-50 start-50 translate-middle"
-                    style={{ minWidth: "400px" }}
-                >
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">S'inscrire</h5>
-                                <button
-                                    // onClick={closeModal}
-                                    className="btn-close"></button>
-                            </div>
+            {modalState.signUpModal && (
+                <div className="position-fixed top-0 vw-100 vh-100">
+                    <div
+                        className="w-100 h-100 bg-dark bg-opacity-75">
+                    </div>
+                    <div
+                        className="position-absolute top-50 start-50 translate-middle"
+                        style={{ minWidth: "400px" }}
+                    >
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title">S'inscrire</h5>
+                                    <button
+                                        onClick={() => toggleModals("close")}
+                                        className="btn-close"></button>
+                                </div>
 
-                            <div className="modal-body">
-                                <form
-                                    // ref={formRef}
-                                    // onSubmit={handleForm}
-                                    className="sign-up-form">
-                                    <div className="mb-3">
-                                        <label htmlFor="signUpEmail" className="form-label">
-                                            Email
-                                        </label>
-                                        <input
-                                            //   ref={addInputs}
-                                            name="email"
-                                            required
-                                            type="email"
-                                            className="form-control"
-                                            id="signUpEmail"
-                                        />
-                                    </div>
+                                <div className="modal-body">
+                                    <form
+                                        // ref={formRef}
+                                        // onSubmit={handleForm}
+                                        className="sign-up-form">
+                                        <div className="mb-3">
+                                            <label htmlFor="signUpEmail" className="form-label">
+                                                Email
+                                            </label>
+                                            <input
+                                                //   ref={addInputs}
+                                                name="email"
+                                                required
+                                                type="email"
+                                                className="form-control"
+                                                id="signUpEmail"
+                                            />
+                                        </div>
 
-                                    <div className="mb-3">
-                                        <label htmlFor="signUpPwd" className="form-label">
-                                            Mot de passe
-                                        </label>
-                                        <input
-                                            // ref={addInputs}
-                                            name="pwd"
-                                            required
-                                            type="password"
-                                            className="form-control"
-                                            id="signUpPwd"
-                                        />
-                                    </div>
+                                        <div className="mb-3">
+                                            <label htmlFor="signUpPwd" className="form-label">
+                                                Mot de passe
+                                            </label>
+                                            <input
+                                                // ref={addInputs}
+                                                name="pwd"
+                                                required
+                                                type="password"
+                                                className="form-control"
+                                                id="signUpPwd"
+                                            />
+                                        </div>
 
-                                    <div className="mb-3">
-                                        <label htmlFor="repeatPwd" className="form-label">
-                                            Confirmer mot de passe
-                                        </label>
-                                        <input
-                                            // ref={addInputs}
-                                            name="pwd"
-                                            required
-                                            type="password"
-                                            className="form-control"
-                                            id="repeatPwd"
-                                        />
-                                        {/* <p className="text-danger mt-1">{validation}</p> */}
-                                    </div>
+                                        <div className="mb-3">
+                                            <label htmlFor="repeatPwd" className="form-label">
+                                                Confirmer mot de passe
+                                            </label>
+                                            <input
+                                                // ref={addInputs}
+                                                name="pwd"
+                                                required
+                                                type="password"
+                                                className="form-control"
+                                                id="repeatPwd"
+                                            />
+                                            {/* <p className="text-danger mt-1">{validation}</p> */}
+                                        </div>
 
-                                    <button className="btn btn-primary">Valider</button>
-                                </form>
+                                        <button className="btn btn-primary"
+                                        >Valider</button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-            </div></>)
+                </div>
+            )}
+        </>)
 }
 
-export default SignUpModal
--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,9 +1,41 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 // je crée le contexte:
 export const UserContext = createContext()
 
 // je crée le composant d'ordre supérieur = PROVIDER qui va retourner le UserContext.Provider :
 export function UserContextProvider(props) {
+    //je définis mon state:
+    const [modalState, setModalState] = useState({
+        signUpModal: true,
+        signInModal: false
+    })
+    //fonction qui gère l'ouvertue des modales au clic sur le bouton:
+    const toggleModals = modal => {
+        if (modal === "signIn") {
+            setModalState({
+                signUpModal: false,
+                signInModal: true
+            })
+        }
+        if (modal === "signUp") {
+            setModalState({
+                signUpModal: true,
+                signInModal: false
+            })
+        }
+        if (modal === "close") {
+            setModalState({
+                signUpModal: false,
+                signInModal: false
+            })
+        }
+    }
 
+    //je passe le state à APP via le provider via la VALUE:
+    return (
+        <UserContext.Provider value={{ modalState, toggleModals }}>
+            {props.children}
+        </UserContext.Provider>
+    )
 }
